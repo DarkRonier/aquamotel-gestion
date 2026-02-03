71ab037a3c7840d253f7be0e60bde89cbf6311ad
DarkRonier <matibru4@gmail.com>
2026-02-03 00:47:28 -0300

    pruebas de datos

--- backend/src/operaciones/operaciones.module.ts
+++ backend/src/operaciones/operaciones.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OperacionesService } from './operaciones.service';
 import { OperacionesController } from './operaciones.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [OperacionesService],
   controllers: [OperacionesController]
 })
--- backend/src/productos/productos.module.ts
+++ backend/src/productos/productos.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductosController } from './productos.controller';
 import { ProductosService } from './productos.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ProductosController],
   providers: [ProductosService]
 })
